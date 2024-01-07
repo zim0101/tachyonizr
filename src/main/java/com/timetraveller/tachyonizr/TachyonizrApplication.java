ee18324d745c07884042aa7dcdbafd6ae9d8778e
zim0101 <cybertronix.4406@gmail.com>
2024-01-07 14:28:52 +0600

    Add minimum folder structure

---
+++ src/main/java/com/timetraveller/tachyonizr/TachyonizrApplication.java
@@ -0,0 +1,13 @@
+package com.timetraveller.tachyonizr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TachyonizrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TachyonizrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timetraveller/tachyonizr/TachyonizrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timetraveller.tachyonizr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TachyonizrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
